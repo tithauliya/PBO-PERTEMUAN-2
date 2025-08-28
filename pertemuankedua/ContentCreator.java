368825f4d5460d5078a703974ed64b9c20445540
tithauliya <tithauliya21@gmail.com>
2025-08-28 12:47:24 +0700

    Add files via upload

---
+++ pertemuankedua/ContentCreator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuankedua;
+
+/**
+ *
+ * @author HP
+ */
+public interface ContentCreator {
+    void membuatKonten();
+    void liveStreaming();
+    void mendapatkanEndorse();
+}
